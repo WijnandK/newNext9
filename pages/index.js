1194766f968af11b14aefb873bd0334e2c1662e8
Wijnand Korlaar <korlaarwijnand@hotmail.com>
2019-10-12 20:42:03 +0200

    First init

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import axios from "axios";
+
+function Home({ products }) {
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  // async function getProducts() {
+  //   console.log(response.data);
+  // }
+
+  return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return { products: response.data };
+};
+
+export default Home;

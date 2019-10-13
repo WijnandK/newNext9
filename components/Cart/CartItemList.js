c5e2e69e74025ee628bfcb793a32c0026511cd0c
Wijnand Korlaar <korlaarwijnand@hotmail.com>
2019-10-13 18:11:22 +0200

    Added cart

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,22 @@
+import { Header, Segment, Button, Icon } from "semantic-ui-react";
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = false;
+  return (
+    <Segment secondary color="teal" inverted textAlign="center" placeholder>
+      <Header icon>
+        <Icon name="shopping basket" />
+        No products in your cart. Add some
+      </Header>
+      <div>
+        {user ? (
+          <Button color="orange">View products</Button>
+        ) : (
+          <Button color="blue">Log in to add products</Button>
+        )}
+      </div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
